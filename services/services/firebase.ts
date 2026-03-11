f885ee29830c201960835d8222690500f61fa8d9
iggysaint <mr.ignatiusarthur@gmail.com>
2026-03-11 16:52:14 +0000

    Initialize Firebase in firebase.ts
    Add Firebase initialization with configuration

---
+++ services/services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6aGidHQEMrLrVCbWagEZorSbrLN3AWUI",
+  authDomain: "campus-app-9a1bf.firebaseapp.com",
+  projectId: "campus-app-9a1bf",
+  storageBucket: "campus-app-9a1bf.firebasestorage.app",
+  messagingSenderId: "163964736987",
+  appId: "1:163964736987:web:c5d48afeb2066a77f743fe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
