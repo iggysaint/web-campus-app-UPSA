cc7fe786999f0020a2057db691e241808a1625af
iggysaint <mr.ignatiusarthur@gmail.com>
2026-03-11 18:48:44 +0000

    Refactor Firebase imports and configuration

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD6aGidHQEMrLrVCbWagEZorSbrLN3AWUI",
   authDomain: "campus-app-9a1bf.firebaseapp.com",
@@ -13,7 +10,6 @@ const firebaseConfig = {
   appId: "1:163964736987:web:c5d48afeb2066a77f743fe"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
